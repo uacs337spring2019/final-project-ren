5fc9816fc6346e530acbe54d7a1670672a5f5ae6
xuanleiren <42553474+xuanleiren@users.noreply.github.com>
2019-04-25 10:45:31 -0700

    Update memo_service.js

--- memo_service.js
+++ memo_service.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers",
